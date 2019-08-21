0c9d85279042fc6a67b5645601bb009595f8c5e3
matt <m.cooper44@gmail.com>
2019-08-21 16:49:22 +0000

    methods for pulling data from db for sms pipepline

--- db_model.py
+++ db_model.py
@@ -2,46 +2,14 @@ import sqlite3
 
 target_number = '+15195555555' # change this string to an actual number 
 
-
-# file ID, app #
-gift_table = [(123456,25), 
-              (321654, 26), 
-              (654987, 100), 
-              (789456, 101), 
-              (369852, 450)]
-
-route_table = [(123456,99), 
-              (321654, 99), 
-              (654987, 100), 
-              (789456, 100), 
-              (369852, 200)]
-
-person_table =  [(123456,'John', 'Doe',f'{target_number}'), 
-              (321654, 'Jane', 'Doe',  f'{target_number}'), 
-              (654987, 'John', 'Smith', f'{target_number}'), 
-              (789456, 'Adam', 'Smith',f'{target_number}'), 
-              (369852, 'Ada', 'Twist', f'{target_number}')]
-
-test_values = {'gift_table': (gift_table,'(?,?)'),
-               'route_table': (route_table,'(?,?)'),
-               'person_table': (person_table,'(?,?,?,?)')}
-
-gift_write = '''CREATE TABLE IF NOT EXISTS gift_table (file_id INT PRIMARY KEY, app_num
-                INT)'''
-
-route_write = '''CREATE TABLE IF NOT EXISTS route_table (file_id INT PRIMARY KEY, 
-                route_num INT)'''
-
-person_write = '''CREATE TABLE IF NOT EXISTS person_table (file_id INT PRIMARY KEY, 
-                 f_name TEXT, l_name TEXT, phone_num TEXT)'''
-
-setup_strings = [gift_write, route_write, person_write]
-
 class Database():
     '''
     for interacting with databases
     path_name is the location and name of the databse to connect with
     or create
+
+    connect() method establishes a database and/or connection to one
+    
 
     '''
     def __init__(self, path_name):
@@ -71,8 +39,9 @@ class Database():
             elif first_time == True and not any(strings):
                 print('no strings provided to provision tables')
 
-        except:
+        except Exception as e:
             print('could not establish connection to database')
+            print(e)
 
 
 
@@ -111,9 +80,26 @@ class Database():
         rows = self.cur.fetchall()
         return rows
 
+    def update(self, string, tple):
+        '''
+        executes sql string with values tple
+        to update a table
+        '''
+        if all((string,tple)):
+            try:
+                self.cur.execute(string, tple)
+                return True
+            except Exception as error:
+                return error
+        else:
+            print('input for SQL update operation is none')
+            return False
+
     def lookup_string(self, string, tple):
         '''
         executes sql string with values tple
+        if tple != None asuming it a fetchone scenario
+        if tple = None, assuming it is a fetchall scenario
         '''
         rows = None
         if tple:
@@ -146,6 +132,11 @@ class database_manager:
     '''
     Interface for the Database class
     contains methods for making specific calls to the database
+
+    dbm = database_manager({'client_db': 'sms_client_test.db', 
+                                'sa': 'sa_2019_appointments.db'})
+    dbm.initialize_connections() # instantiate db objects
+
     '''
 
 
@@ -172,6 +163,10 @@ class database_manager:
         param d_struct is a dictionary as per doc string described
         on insert method of the database class
 
+        'table name': ([(values_to_write,)], # list of tuple(s)
+                       '(?, [...])') # string tuple with a ? for each 
+                                     # value to insert
+
         '''
         self.db_struct[db_name].insert(d_struct)
 
@@ -179,17 +174,50 @@ class database_manager:
         ls = 'SELECT app_num FROM gift_table WHERE file_id=?'
         return self.db_struct[database].lookup_string(ls, file_id)
 
+    def return_app_range(self, database, low, high):
+        '''
+        return a range of gift appointments between the low and high values
+        used to lookup file_id's that have upcoming gift appointments
+        returns 2 tuples of file_id, app_num
+        '''
+        ls = f'''SELECT gift_table.file_id, app_num, phone_num
+                 FROM gift_table 
+                 INNER JOIN person_table ON gift_table.file_id =
+                 person_table.file_id WHERE 
+                 app_num <= {high} AND app_num >= {low} AND message_sent = 0
+                 AND person_table.phone_num IS NOT NULL'''
+        return self.db_struct[database].lookup_string(ls, None)
+    
+    def return_route_range(self, database, low, high):
+        '''
+        return a range of file_id's that have route numbers between parameter
+        low and high - used to find households that have an upcoming delivery
+        returns 2 tuples of file_id, route_num
+        '''
+        ls = f'''SELECT route_table.file_id, route_num, phone_num FROM 
+                 route_table 
+                 INNER JOIN person_table
+                 ON person_table.file_id = route_table.file_id
+                 WHERE route_table.route_num >= {low} AND 
+                 route_num <= {high} AND route_table.message_sent = 0
+                 AND person_table.phone_num IS NOT NULL'''
+        return self.db_struct[database].lookup_string(ls, None)
+
     def return_route_num(self, database, file_id):
         ls = 'SELECT route_num FROM route_table WHERE file_id=?'
         return self.db_struct[database].lookup_string(ls, (file_id,))
 
     def return_file_ids(self, database):
         '''
-        returns tuples of (file_id, f_name, l_name, phone_num)
+        returns tuples of (file_id, cell_num)
         from the person_table of the client_database
-        
+        for all the Households that have a valid NOT NULL cell_num value
+        typical usage would be dbm.return_file_ids('client_database.db')
+        or in testing scenarios .return_file_ids('sms_client_test.db')
+
         '''
-        ls = 'SELECT file_id, f_name, l_name FROM person_table'
+        ls = '''SELECT file_id, cell_num FROM person_table 
+                WHERE cell_num IS NOT NULL'''
         return self.db_struct[database].lookup_string(ls, None)
     
     def return_app_date(self, database, app_num, echo=False):
@@ -202,10 +230,34 @@ class database_manager:
         if echo: print(f'found {day_time}')
         return day_time
 
+    def return_gift_sent(self, database, app_number):
+        '''
+        looks in database in the 'gift_table' to see if 'message_sent' where
+        'app_num' = parameter app_number
+        '''
+        ls = 'SELECT message_sent FROM gift_table WHERE app_num=?'
+        return self.db_struct[database].lookup_string(ls, (app_number,))
 
-if __name__ == '__main__':
-    # connect and initialize a test database
-    test_db = Database('sms_client_test.db')
-    test_db.connect(first_time=True, strings=setup_strings)
-    test_db.insert(test_values)
-    test_db.close()
+    def set_gift_sent(self, database, file_id):
+        '''
+        set the message_sent value in the gift_table at param file_id
+        '''
+        ls = 'UPDATE gift_table SET message_sent=1 WHERE file_id=?'
+        try:
+            self.db_struct[database].update(ls, (file_id,))
+            return True
+        except Exception as error:
+            print(error)
+            return False
+        
+    def set_route_sent(self, database, file_id):
+        '''
+        set the message_sent value in the route table at param file_id
+        '''
+        try:
+            self.db_struct[database].update(ls, (file_id,))
+            return True
+        except Exception as error:
+            print(error)
+            return False
+
